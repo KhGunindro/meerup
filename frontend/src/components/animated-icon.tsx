34a99e10360f4706fba9e0435aa7235ccf579bd3
GripsonTH <gripsonthongam@gmail.com>
2026-09-23 13:01:17 +0530

    change splash logo

--- frontend/src/components/animated-icon.tsx
+++ frontend/src/components/animated-icon.tsx
@@ -44,7 +44,7 @@ export function AnimatedSplashOverlay() {
     },
   });
 
-  const image = <Image style={styles.image} source={require('@/assets/images/expo-logo.png')} />;
+  const image = <Image style={styles.image} contentFit="contain" source={require('@/assets/images/logo.png')} />;
 
   return animate ? (
     <Animated.View
@@ -109,13 +109,8 @@ const glowKeyframe = new Keyframe({
 export function AnimatedIcon() {
   return (
     <View style={styles.iconContainer}>
-      <Animated.View entering={glowKeyframe.duration(60 * 1000 * 4)} style={styles.glow}>
-        <Image style={styles.glow} source={require('@/assets/images/logo-glow.png')} />
-      </Animated.View>
-
-      <Animated.View entering={keyframe.duration(DURATION)} style={styles.background} />
       <Animated.View style={styles.imageContainer} entering={logoKeyframe.duration(DURATION)}>
-        <Image style={styles.image} source={require('@/assets/images/expo-logo.png')} />
+        <Image style={styles.image} contentFit="contain" source={require('@/assets/images/logo.png')} />
       </Animated.View>
     </View>
   );
@@ -139,8 +134,8 @@ const styles = StyleSheet.create({
     zIndex: 100,
   },
   image: {
-    width: 76,
-    height: 71,
+    width: 140,
+    height: 140,
   },
   background: {
     borderRadius: 40,
@@ -151,7 +146,7 @@ const styles = StyleSheet.create({
   },
   splashOverlay: {
     ...StyleSheet.absoluteFill,
-    backgroundColor: '#208AEF',
+    backgroundColor: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
     zIndex: 1000,
